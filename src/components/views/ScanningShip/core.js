8fc15e50c0a2ade1a0c0cad5d8ea074027f9f725
mweaver <mweaver@csod.com>
2020-06-13 01:24:30 -0600

    fixed the issue

--- src/components/views/ScanningShip/core.js
+++ src/components/views/ScanningShip/core.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PictureUpload from "./core/PictureUpload";
 import gql from "graphql-tag.macro";
-import {useQuery} from "react-apollo";
-import {useSubscribeToMore} from "helpers/hooks/useQueryAndSubscribe";
+import useQueryAndSubscription from "helpers/hooks/useQueryAndSubscribe";
 import FireManagementTable from "./core/Table";
 
 const GET_INITIAL = gql`
@@ -47,30 +46,21 @@ const SUBSCRIBE_LAYOUT = gql`
 
 const ScanningShipCore = props => {
   const {simulator} = props;
-  const {loading, data, subscribeToMore} = useQuery(GET_INITIAL, {
-    variables: {simulatorId: simulator.id},
-  });
-  const subConfig = React.useMemo(
-    () => ({
-      variables: {simulatorId: simulator.id},
-      updateQuery: (previousResult, {subscriptionData}) => ({
-        ...previousResult,
-        FireLayout: subscriptionData.data.FireLayout,
-      }),
-    }),
-    [simulator.id],
+  const {loading, data} = useQueryAndSubscription(
+    {query: GET_INITIAL, variables: {simulatorId: simulator.id}},
+    {query: SUBSCRIBE_LAYOUT, variables: {simulatorId: simulator.id}},
   );
-  useSubscribeToMore(subscribeToMore, SUBSCRIBE_LAYOUT, subConfig);
-  if (loading || !data) return null;
-  let fireLayout = {fireDecks: []};
-  if (data.FireLayout) {
-    fireLayout = data.FireLayout[0];
-  }
+
+  if (!data || loading) return null;
+  const {FireLayout} = data;
 
   return (
     <div>
       <PictureUpload />
-      <FireManagementTable simulatorId={simulator.id} fireLayout={fireLayout} />
+      <FireManagementTable
+        simulatorId={simulator.id}
+        fireLayout={FireLayout[0]}
+      />
     </div>
   );
 };
--- src/components/views/ScanningShip/index.js
+++ src/components/views/ScanningShip/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import gql from "graphql-tag.macro";
-import {useQuery} from "react-apollo";
-import {useSubscribeToMore} from "helpers/hooks/useQueryAndSubscribe";
+import useQueryAndSubscription from "helpers/hooks/useQueryAndSubscribe";
 import FireTable from "./view/FireTable.js";
 const GET_INITIAL = gql`
   query GET_INITIAL($simulatorId: ID!) {
@@ -45,29 +44,17 @@ const SUBSCRIBE_LAYOUT = gql`
 
 const ScanningShip = props => {
   const {simulator} = props;
-  const {loading, data, subscribeToMore} = useQuery(GET_INITIAL, {
-    variables: {simulatorId: simulator.id},
-  });
-  const subConfig = React.useMemo(
-    () => ({
-      variables: {simulatorId: simulator.id},
-      updateQuery: (previousResult, {subscriptionData}) => ({
-        ...previousResult,
-        FireLayout: subscriptionData.data.FireLayout,
-      }),
-    }),
-    [simulator.id],
+  const {loading, data} = useQueryAndSubscription(
+    {query: GET_INITIAL, variables: {simulatorId: simulator.id}},
+    {query: SUBSCRIBE_LAYOUT, variables: {simulatorId: simulator.id}},
   );
-  useSubscribeToMore(subscribeToMore, SUBSCRIBE_LAYOUT, subConfig);
-  if (loading || !data) return null;
-  let fireLayout = {fireDecks: []};
-  if (data.FireLayout) {
-    fireLayout = data.FireLayout[0];
-  }
+
+  if (!data || loading) return null;
+  const {FireLayout} = data;
 
   return (
     <div>
-      <FireTable fireLayout={fireLayout} />
+      <FireTable fireLayout={FireLayout[0]} />
     </div>
   );
 };
